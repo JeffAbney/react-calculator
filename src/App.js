8247dd1554ba6c63eaf6f90c11bd500e2845879c
JeffAbney <jeffmabney@gmail.com>
2018-09-12 16:35:59 -0500

    add basic structure and methods

--- src/App.js
+++ src/App.js
@@ -3,16 +3,80 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			input: 0,
+			output: "",
+			decimal: false,
+			frontZero: false,
+			lastOperator: "",
+		}
+
+		this.handleNumberInput = this.handleNumberInput.bind(this);
+		this.handleOperatorInput = this.handleOperatorInput.bind(this);
+		this.handleClear = this.handleClear.bind(this);
+	}
+
+	handleNumberInput(){
+
+	}
+
+	handleOperatorInput(){
+
+	}
+
+	handleClear(){
+
+	}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="calculator">
+        	<div className="display" id="display">{this.state.input}
+        	</div>
+        	<div className="input-container">
+
+        		<div className="number-button" id="one" onClick={()=>this.handleNumberInput()} >1
+        		</div>
+        		<div className="number-button" id="two" onClick={()=>this.handleNumberInput()} >2
+        		</div>
+        		<div className="number-button" id="three" onClick={()=>this.handleNumberInput()} >3
+        		</div>
+        		<div className="number-button" id="four" onClick={()=>this.handleNumberInput()} >4
+        		</div>
+        		<div className="number-button" id="five" onClick={()=>this.handleNumberInput()} >5
+        		</div>
+        		<div className="number-button" id="six" onClick={()=>this.handleNumberInput()} >6
+        		</div>
+        		<div className="number-button" id="seven" onClick={()=>this.handleNumberInput()} >7
+        		</div>
+        		<div className="number-button" id="eight" onClick={()=>this.handleNumberInput()} >8
+        		</div>
+        		<div className="number-button" id="nine" onClick={()=>this.handleNumberInput()} >9
+        		</div>
+        		<div className="number-button" id="zero" onClick={()=>this.handleNumberInput()} >0
+        		</div>
+        		<div className="number-button" id="decimal" onClick={()=>this.handleNumberInput()} >.
+        		</div>
+        		<div className="operator-button" id="add" onClick={()=>this.handleOperatorInput()}>+
+        		</div>
+        		<div className="operator-button" id="subtract" onClick={()=>this.handleOperatorInput()}>-
+        		</div>
+        		<div className="operator-button" id="multiply" onClick={()=>this.handleOperatorInput()}>*
+        		</div>
+        		<div className="operator-button" id="divide" onClick={()=>this.handleOperatorInput()}>/
+        		</div>
+        		<div className="operator-button" id="equals" onClick={()=>this.handleOperatorInput()}>=
+        		</div>
+        		<div className="clear-button" id="clear" onClick={()=>this.handleClear()}>C
+        		</div>
+
+        	</div>
+        </div>
       </div>
     );
   }
