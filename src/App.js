0de5043efe207ca320b14b492aec2f48a97e162e
JeffAbney <jeffmabney@gmail.com>
2018-09-14 12:32:05 -0500

    build functions, add input display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +7,8 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			input: 0,
-			output: "",
-			decimal: false,
-			frontZero: false,
+			display: "0",
+			input: "",
 			lastOperator: "",
 		}
 
@@ -20,16 +17,36 @@ class App extends Component {
 		this.handleClear = this.handleClear.bind(this);
 	}
 
-	handleNumberInput(){
-
+	handleNumberInput(val){
+		const decimalRegex = /\./;
+		const decimalCheck = decimalRegex.test(this.state.display);
+		const operatorRegex = /[\+\-\*\/]$/;
+		const operatorCheck = operatorRegex.test(this.state.input);
+		this.setState({
+			display: operatorCheck ? val : this.state.display === "0" ? val : val !== "." ? this.state.display + val
+			        : decimalCheck ? this.state.display : this.state.display + val,
+			input: this.state.display === "0" ? val : val !== "." ? this.state.input + val
+			        : decimalCheck ? this.state.input : this.state.input + val,
+		})
 	}
 
-	handleOperatorInput(){
-
+	handleOperatorInput(val){
+		const operatorRegex = /[\+\-\*\/]$/;
+		const operatorCheck = operatorRegex.test(this.state.input);
+		val === "=" ? operatorCheck ? 
+		        this.setState({input: eval(this.state.input.slice(0, -1)), display: eval(this.state.input.slice(0, -1))})
+		                :this.setState({display: eval(this.state.input), input:eval(this.state.input)}) 
+		                        :operatorCheck ? this.setState({input: this.state.input.slice(0,-1).concat(val)})
+		                                :this.setState({ input: this.state.input + val});
+			
+		
 	}
 
 	handleClear(){
-
+		this.setState({
+			display: "0",
+			input: "",
+		})
 	}
 
   render() {
@@ -38,39 +55,40 @@ class App extends Component {
         <div className="calculator">
         	<div className="solar-panel" id="solar-panel">
         	</div>
-        	<div className="display" id="display">{this.state.input}
+        	<div className="input-display">{this.state.input}</div>
+        	<div className="display" id="display">{this.state.display}
         	</div>
-        	<div className="calc-button number-button" id="seven" onClick={()=>this.handleNumberInput()} >7
+        	<div className="calc-button number-button" id="seven" onClick={()=>this.handleNumberInput("7")} >7
         	</div>
-        	<div className="calc-button number-button" id="eight" onClick={()=>this.handleNumberInput()} >8
+        	<div className="calc-button number-button" id="eight" onClick={()=>this.handleNumberInput("8")} >8
         	</div>
-        	<div className="calc-button number-button" id="nine" onClick={()=>this.handleNumberInput()} >9
+        	<div className="calc-button number-button" id="nine" onClick={()=>this.handleNumberInput("9")} >9
         	</div>
-        	<div className="calc-button number-button" id="four" onClick={()=>this.handleNumberInput()} >4
+        	<div className="calc-button number-button" id="four" onClick={()=>this.handleNumberInput("4")} >4
         	</div>
-        	<div className="calc-button number-button" id="five" onClick={()=>this.handleNumberInput()} >5
+        	<div className="calc-button number-button" id="five" onClick={()=>this.handleNumberInput("5")} >5
         	</div>
-        	<div className="calc-button number-button" id="six" onClick={()=>this.handleNumberInput()} >6
+        	<div className="calc-button number-button" id="six" onClick={()=>this.handleNumberInput("6")} >6
         	</div>
-        	<div className="calc-button number-button" id="one" onClick={()=>this.handleNumberInput()} >1
+        	<div className="calc-button number-button" id="one" onClick={()=>this.handleNumberInput("1")} >1
         	</div>
-        	<div className="calc-button number-button" id="two" onClick={()=>this.handleNumberInput()} >2
+        	<div className="calc-button number-button" id="two" onClick={()=>this.handleNumberInput("2")} >2
         	</div>
-        	<div className="calc-button number-button" id="three" onClick={()=>this.handleNumberInput()} >3
+        	<div className="calc-button number-button" id="three" onClick={()=>this.handleNumberInput("3")} >3
         	</div>
-        	<div className="calc-button number-button" id="zero" onClick={()=>this.handleNumberInput()} >0
+        	<div className="calc-button number-button" id="zero" onClick={()=>this.handleNumberInput("0")} >0
         	</div>
-        	<div className="calc-button number-button" id="dec" onClick={()=>this.handleNumberInput()} >.
+        	<div className="calc-button number-button" id="dec" onClick={()=>this.handleNumberInput(".")} >.
         	</div>
-        	<div className="calc-button operator-button" id="add" onClick={()=>this.handleOperatorInput()}>+
+        	<div className="calc-button operator-button" id="add" onClick={()=>this.handleOperatorInput("+")}>+
         	</div>
-        	<div className="calc-button operator-button" id="subt" onClick={()=>this.handleOperatorInput()}>-
+        	<div className="calc-button operator-button" id="subt" onClick={()=>this.handleOperatorInput("-")}>-
         	</div>
-        	<div className="calc-button operator-button" id="mult" onClick={()=>this.handleOperatorInput()}>*
+        	<div className="calc-button operator-button" id="mult" onClick={()=>this.handleOperatorInput("*")}>*
         	</div>
-        	<div className="calc-button operator-button" id="div" onClick={()=>this.handleOperatorInput()}>/
+        	<div className="calc-button operator-button" id="div" onClick={()=>this.handleOperatorInput("/")}>/
         	</div>
-        	<div className="calc-button operator-button" id="equ" onClick={()=>this.handleOperatorInput()}>=
+        	<div className="calc-button operator-button" id="equ" onClick={()=>this.handleOperatorInput("=")}>=
         	</div>
         	<div className="calc-button clear-button" id="clear" onClick={()=>this.handleClear()}>C
         	</div>
