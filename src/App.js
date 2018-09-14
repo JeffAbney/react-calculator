d7aae86cce12fb86900f1a4027b048c1354901a2
JeffAbney <jeffmabney@gmail.com>
2018-09-14 09:43:29 -0500

    create grid and layout

--- src/App.js
+++ src/App.js
@@ -36,45 +36,43 @@ class App extends Component {
     return (
       <div className="App">
         <div className="calculator">
+        	<div className="solar-panel" id="solar-panel">
+        	</div>
         	<div className="display" id="display">{this.state.input}
         	</div>
-        	<div className="input-container">
-
-        		<div className="number-button" id="one" onClick={()=>this.handleNumberInput()} >1
-        		</div>
-        		<div className="number-button" id="two" onClick={()=>this.handleNumberInput()} >2
-        		</div>
-        		<div className="number-button" id="three" onClick={()=>this.handleNumberInput()} >3
-        		</div>
-        		<div className="number-button" id="four" onClick={()=>this.handleNumberInput()} >4
-        		</div>
-        		<div className="number-button" id="five" onClick={()=>this.handleNumberInput()} >5
-        		</div>
-        		<div className="number-button" id="six" onClick={()=>this.handleNumberInput()} >6
-        		</div>
-        		<div className="number-button" id="seven" onClick={()=>this.handleNumberInput()} >7
-        		</div>
-        		<div className="number-button" id="eight" onClick={()=>this.handleNumberInput()} >8
-        		</div>
-        		<div className="number-button" id="nine" onClick={()=>this.handleNumberInput()} >9
-        		</div>
-        		<div className="number-button" id="zero" onClick={()=>this.handleNumberInput()} >0
-        		</div>
-        		<div className="number-button" id="decimal" onClick={()=>this.handleNumberInput()} >.
-        		</div>
-        		<div className="operator-button" id="add" onClick={()=>this.handleOperatorInput()}>+
-        		</div>
-        		<div className="operator-button" id="subtract" onClick={()=>this.handleOperatorInput()}>-
-        		</div>
-        		<div className="operator-button" id="multiply" onClick={()=>this.handleOperatorInput()}>*
-        		</div>
-        		<div className="operator-button" id="divide" onClick={()=>this.handleOperatorInput()}>/
-        		</div>
-        		<div className="operator-button" id="equals" onClick={()=>this.handleOperatorInput()}>=
-        		</div>
-        		<div className="clear-button" id="clear" onClick={()=>this.handleClear()}>C
-        		</div>
-
+        	<div className="calc-button number-button" id="seven" onClick={()=>this.handleNumberInput()} >7
+        	</div>
+        	<div className="calc-button number-button" id="eight" onClick={()=>this.handleNumberInput()} >8
+        	</div>
+        	<div className="calc-button number-button" id="nine" onClick={()=>this.handleNumberInput()} >9
+        	</div>
+        	<div className="calc-button number-button" id="four" onClick={()=>this.handleNumberInput()} >4
+        	</div>
+        	<div className="calc-button number-button" id="five" onClick={()=>this.handleNumberInput()} >5
+        	</div>
+        	<div className="calc-button number-button" id="six" onClick={()=>this.handleNumberInput()} >6
+        	</div>
+        	<div className="calc-button number-button" id="one" onClick={()=>this.handleNumberInput()} >1
+        	</div>
+        	<div className="calc-button number-button" id="two" onClick={()=>this.handleNumberInput()} >2
+        	</div>
+        	<div className="calc-button number-button" id="three" onClick={()=>this.handleNumberInput()} >3
+        	</div>
+        	<div className="calc-button number-button" id="zero" onClick={()=>this.handleNumberInput()} >0
+        	</div>
+        	<div className="calc-button number-button" id="dec" onClick={()=>this.handleNumberInput()} >.
+        	</div>
+        	<div className="calc-button operator-button" id="add" onClick={()=>this.handleOperatorInput()}>+
+        	</div>
+        	<div className="calc-button operator-button" id="subt" onClick={()=>this.handleOperatorInput()}>-
+        	</div>
+        	<div className="calc-button operator-button" id="mult" onClick={()=>this.handleOperatorInput()}>*
+        	</div>
+        	<div className="calc-button operator-button" id="div" onClick={()=>this.handleOperatorInput()}>/
+        	</div>
+        	<div className="calc-button operator-button" id="equ" onClick={()=>this.handleOperatorInput()}>=
+        	</div>
+        	<div className="calc-button clear-button" id="clear" onClick={()=>this.handleClear()}>C
         	</div>
         </div>
       </div>
