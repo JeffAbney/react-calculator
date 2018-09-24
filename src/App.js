17e17690dff6dcbf41fb184e0b3cf294063a64ab
JeffAbney <jeffmabney@gmail.com>
2018-09-24 09:58:22 -0500

    Finish componentizing key-pad.

--- src/App.js
+++ src/App.js
@@ -14,62 +14,147 @@ const SolarPanel = (props) => {
 };
 
 const Display = (props) => {
-  const displayClass = "display " + props.state.fade;
-  const inputClass = "input-display " + props.state.fade;
+  const { fade, display, input } = props.state;
+  const displayClass = `display ${fade}`;
+  const inputClass = `input-display ${fade}`;
 	return (
 	  <div className="display-container">
 	    <div className={inputClass}>
-          <p>{props.state.input}</p>
+          <p>{input}</p>
         </div>
-        <div className={displayClass} id="display">{props.state.display.length > 10 ? Number(props.state.display).toExponential(6) : props.state.display}
+        <div className={displayClass} id="display">{display.length > 10 ? Number(display).toExponential(6) : display}
         </div>
-        <div className="calc-button clear-button" id="clear" onClick={props.onClick}>C
+        <div 
+            className="calc-button clear-button"
+            id="clear"
+            onClick={props.onClick}>
+            C
         </div>
       </div>
 	)
 };
 
 const NumberPad = (props) => {
+	const { handleNumberInput, handleOperatorInput } = props;
 	return (
   <div className="number-pad">
-    <div className="calc-button number-button" id="seven" onClick={()=>props.handleNumberInput("7")} >7
+    <div 
+        className="calc-button number-button"
+        id="seven" 
+        onClick={() => handleNumberInput("7")} 
+    >
+      7
     </div>
-    <div className="calc-button number-button" id="eight" onClick={()=>props.handleNumberInput("8")} >8
+    <div 
+        className="calc-button number-button"
+        id="eight" 
+        onClick={() => handleNumberInput("8")} 
+    >
+      8
     </div>
-    <div className="calc-button number-button" id="nine" onClick={()=>props.handleNumberInput("9")} >9
+    <div 
+        className="calc-button number-button" 
+        id="nine" 
+        onClick={() => handleNumberInput("9")} 
+    >
+      9
     </div>
-    <div className="calc-button number-button" id="four" onClick={()=>props.handleNumberInput("4")} >4
+    <div 
+        className="calc-button number-button" 
+        id="four" 
+        onClick={() => handleNumberInput("4")} 
+    >
+      4
     </div>
-    <div className="calc-button number-button" id="five" onClick={()=>props.handleNumberInput("5")} >5
+    <div 
+        className="calc-button number-button" 
+        id="five" 
+        onClick={() => handleNumberInput("5")} 
+    >
+      5
     </div>
-    <div className="calc-button number-button" id="six" onClick={()=>props.handleNumberInput("6")} >6
+    <div 
+        className="calc-button number-button" 
+        id="six" 
+        onClick={() => handleNumberInput("6")} 
+    >
+      6
     </div>
-    <div className="calc-button number-button" id="one" onClick={()=>props.handleNumberInput("1")} >1
+    <div
+       className="calc-button number-button" 
+       id="one" 
+       onClick={() => handleNumberInput("1")} 
+    >
+      1
     </div>
-    <div className="calc-button number-button" id="two" onClick={()=>props.handleNumberInput("2")} >2
+    <div 
+        className="calc-button number-button" 
+        id="two" 
+        onClick={() => handleNumberInput("2")} 
+    >
+      2
     </div>
-    <div className="calc-button number-button" id="three" onClick={()=>props.handleNumberInput("3")} >3
+    <div 
+        className="calc-button number-button" 
+        id="three" 
+        onClick={() => handleNumberInput("3")} 
+    >
+      3
     </div>
-    <div className="calc-button number-button" id="zero" onClick={()=>props.handleNumberInput("0")} >0
+    <div 
+        className="calc-button number-button" 
+        id="zero" 
+        onClick={() => handleNumberInput("0")} 
+    >
+      0
     </div>
-    <div className="calc-button number-button" id="decimal" onClick={()=>props.handleNumberInput(".")} >.
+    <div 
+        className="calc-button number-button" 
+        id="decimal" 
+        onClick={() => handleNumberInput(".")} 
+    >
+      .
     </div>
-    <div className="calc-button operator-button" id="add" onClick={()=>props.handleOperatorInput("+")}>+
+    <div 
+        className="calc-button operator-button" 
+        id="add" 
+        onClick={() => handleOperatorInput("+")}
+    >
+      +
     </div>
-    <div className="calc-button operator-button" id="subtract" onClick={()=>props.handleOperatorInput("-")}>-
+    <div 
+        className="calc-button operator-button" 
+        id="subtract" 
+        onClick={() => handleOperatorInput("-")}
+    >
+      -
     </div>
-    <div className="calc-button operator-button" id="multiply" onClick={()=>props.handleOperatorInput("*")}>*
+    <div 
+        className="calc-button operator-button" 
+        id="multiply" 
+        onClick={() => handleOperatorInput("*")}
+    >
+      *
     </div>
-    <div className="calc-button operator-button" id="divide" onClick={()=>props.handleOperatorInput("/")}>/
+    <div 
+        className="calc-button operator-button" 
+        id="divide" 
+        onClick={() => handleOperatorInput("/")}
+    >
+      /
     </div>
-    <div className="calc-button operator-button" id="equals" onClick={()=>props.handleOperatorInput("=")}>=
+    <div 
+        className="calc-button operator-button" 
+        id="equals" 
+        onClick={() => handleOperatorInput("=")}
+    >
+      =
     </div>	
   </div>
 )}
 
 /* ----------------------- Main App ------------------------- */
 class App extends Component {
-
     constructor(props) {
         super(props);
 
@@ -107,25 +192,26 @@ class App extends Component {
         const endsInOperator = operatorRegex.test(this.state.input);
         if (endsInOperator || this.state.display === "0") {
             return val;
-        } else if (val !== "." || !hasDecimal) {
+        }
+        if (val !== "." || !hasDecimal) {
             return this.state.display + val;
-            } else {
-                return this.state.display;
             }
+        return this.state.display; 
     }
 
     setInput(val) {
+    	const { input } = this.state;
         const decimalRegex = /\./;
         const hasDecimal = decimalRegex.test(this.state.display);
         const operatorRegex = /[+ \- * / ]$/;
-        const endsInOperator = operatorRegex.test(this.state.input);
+        const endsInOperator = operatorRegex.test(input);
 
-        if (this.state.input === "") {
+        if (input === "") {
             return val;
         } else if (val !== "." || !hasDecimal || endsInOperator) {
-            return this.state.input + val;
+            return input + val;
         } else {
-            return this.state.input;
+            return input;
         }
     }
 
@@ -136,37 +222,47 @@ class App extends Component {
         });
     }
 
+
     handleOperatorInput(val) {
+    	const { input } = this.state;
         const operatorRegex = /[+ \- * /]$/;
         const numRegex = /[\d]/;
-        const previousCharIsNum = numRegex.test(this.state.input);
-        const endsInOperator = operatorRegex.test(this.state.input);
-        function evil(fn) {
-            return new Function('return ' + fn)();
-        }
+        const previousCharIsNum = numRegex.test(input);
+        const endsInOperator = operatorRegex.test(input);
+        const callBackFunc = (val) => new Function('return ' + val);
+        const evil = (inputVal) => (callBackFunc(inputVal));
+
 
         if (val === "=") {
             if (!endsInOperator) {
                 this.setState({
-                	display: (Math.round(evil(this.state.input) * 1000000) / 1000000).toString(),
-                    input: (Math.round(evil(this.state.input) * 1000000) / 1000000).toString()});
+                    input: (
+                    	Math.round(evil(input)() * 1000000) / 1000000
+                    	).toString(),
+                    display: (
+                    	Math.round(evil(input)() * 1000000) / 1000000
+                    	).toString()
+                });
             } else if (previousCharIsNum) {
                 this.setState({
-                	input: (Math.round(evil(this.state.input.slice(0, -1)) * 1000000) / 1000000).toString(),
-                    display: (Math.round(evil(this.state.input.slice(0, -1)) * 1000000) / 1000000).toString()});
+                	input: (
+                	    Math.round(evil(input.slice(0, -1))() * 1000000) / 1000000
+                	).toString(),
+                    display: (
+                    	Math.round(evil(input.slice(0, -1))() * 1000000) / 1000000
+                    ).toString()
+                });
             } else {
                 this.setState({input: "", display: "0"});
             }
-        }
-
-        else {
-            if (endsInOperator) {
-                this.setState({input: this.state.input.slice(0, -1).concat(val)});
-            } else {
-                this.setState({input: this.state.input + val})};
-        }            
+        } else if (endsInOperator) {
+            this.setState({input: input.slice(0, -1).concat(val)});
+        } else {
+                this.setState({input: input + val});
+        }        
     }
 
+      
     handleClear() {
         this.setState({
             display: "0",
@@ -180,10 +276,19 @@ class App extends Component {
         return (
             <div className="App">
               <div className="calculator">
-                <SolarPanel mouseOver={this.handleSolarMouseOn} mouseOff={this.handleSolarMouseOff} />
-                <Display state={this.state} onClick={this.handleClear}/>
-                <NumberPad state={this.state} handleNumberInput={(e)=>this.handleNumberInput(e)}
-                   handleOperatorInput={(e)=>this.handleOperatorInput(e)}/>
+                <SolarPanel
+                    mouseOver={this.handleSolarMouseOn}
+                    mouseOff={this.handleSolarMouseOff}
+                />
+                <Display 
+                    state={this.state} 
+                    onClick={this.handleClear}
+                />
+                <NumberPad 
+                    state={this.state} 
+                    handleNumberInput={(e)=>this.handleNumberInput(e)}
+                    handleOperatorInput={(e)=>this.handleOperatorInput(e)}
+                />
               </div>
             </div>
         );
